0bfc34eeeadc98eaf8c760b68ee75fb24fa76416
brkozkn999 <ibrahimberkozkan34@gmail.com>
2024-06-04 00:00:27 +0300

    initial commit

---
+++ src/main/java/tr/edu/medipol/Task.java
@@ -0,0 +1,21 @@
+package tr.edu.medipol;
+
+public class Task {
+    String task;
+
+    // Default constructor
+    public Task() {
+    }
+
+    public Task(String task) {
+        this.task = task;
+    }
+
+    public String getTask() {
+        return task;
+    }
+
+    public void setTask(String task) {
+        this.task = task;
+    }
+}
